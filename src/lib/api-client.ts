36ac2b93a7f9a0f2ae1efd6b9e173e724c9c9e2d
Mirzohiddev006 <ibrohimjonovm2006@gmail.com>
2025-12-17 08:42:34 +0500

    update-links

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -6,7 +6,7 @@ import toast from "react-hot-toast";
 
 const getApiUrl = () => {
   // In development, use proxy to avoid CORS issues
-  return "https://knitting-ceramic-elect-vision.trycloudflare.com/";
+  return "https://dry-greatest-signals-marine.trycloudflare.com/";
 };
 
 // Helper function to get translated message
